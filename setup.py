0a0bcc5eb721cb1438c635d100940eb7933dac91
Kenneth <imitation@gmail.com>
2012-11-04 22:23:14 +0000

    Added index & setup python scripts

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+import sqlite3
+import sys
+
+con = None
+
+tableSQL = """
+
+CREATE TABLE temperatures
+(
+	id INTEGER PRIMARY KEY,
+	rawTempInside TEXT,
+	rawTempOutside TEXT,
+	tempInside REAL,
+	tempOutside TEMP,
+	timestamp INTEGER
+);
+
+"""
+
+print "Initialising tables:"
+try:
+	con = sqlite3.connect("temp.db")
+	cur = con.cursor()
+	cur.execute(tableSQL);
+	print "Complete."
+
+except sqlite3.Error, e:
+	print "Error: %s when inserting tables" % e.args[0]
+	sys.exit(1)
+finally:
+	if con:
+		con.close()
